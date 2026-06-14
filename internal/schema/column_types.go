6bebcf1c585fba217e9f303b2f15af813acb88dd
Quack Pot <skyler32702@gmail.com>
2026-06-13 19:41:32 -0500

    Base Column Types Defined

--- internal/schema/column_types.go
+++ internal/schema/column_types.go
@@ -1,7 +1,52 @@
 package schema
 
-type Column interface {
-	GenerateMeta() ColumnMeta
-}
+// Integer Columns ------------------------------------------------------------
 
 type IntColumn[C Constraint] struct{}
+type SmallIntColumn[C Constraint] struct{}
+type BigIntColumn[C Constraint] struct{}
+
+// Decimal Columns ------------------------------------------------------------
+
+type FloatColumn[C Constraint] struct{}
+type DoubleColumn[C Constraint] struct{}
+
+// Monetary Columns -----------------------------------------------------------
+
+type MoneyColumn[C Constraint] struct{}
+
+// Boolean Columns ------------------------------------------------------------
+
+type BooleanColumn[C Constraint] struct{}
+
+// Character Columns ----------------------------------------------------------
+
+type TextColumn[C Constraint] struct{}
+
+// Time Columns ---------------------------------------------------------------
+
+type DateColumn[C Constraint] struct{}
+type TimeColumn[C Constraint] struct{}
+type DateTimeColumn[C Constraint] struct{}
+
+type DateZoneColumn[C Constraint] struct{}
+type TimeZoneColumn[C Constraint] struct{}
+type DateTimeZoneColumn[C Constraint] struct{}
+
+type IntervalColumn[C Constraint] struct{}
+
+// Json Columns ---------------------------------------------------------------
+
+type JsonColumn[C Constraint] struct{}
+type JsonBinaryColumn[C Constraint] struct{}
+
+// Network Columns ------------------------------------------------------------
+
+type CidrColumn[C Constraint] struct{}
+type INetColumn[C Constraint] struct{}
+type MacAddress6Column[C Constraint] struct{}
+type MacAddress8Column[C Constraint] struct{}
+
+// UUID Columns ---------------------------------------------------------------
+
+type UuidColumn[C Constraint] struct{}
