c4adb270c1ca5b99083f919b6f130e040f83d1d9
quack-pot <skyler32702@gmail.com>
2026-06-17 09:15:06 -0500

    Expression and generic predicates

--- internal/schema/query.go
+++ internal/schema/query.go
@@ -2,6 +2,14 @@ package schema
 
 import "github.com/quack-pot/quandry/internal/core"
 
+type SqlExpression interface {
+	Evaluate() string
+}
+
+type SqlPredicate[T any] struct {
+	Expression SqlExpression
+}
+
 type QueryBuilder struct {
 }
 
